3acc8972602091dc07de41f6189ba072a0c8e681
jason_tung <yokarilight@gmail.com>
2024-03-19 23:48:03 +0800

    feature: add delete input feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';              
+import { useState } from 'react';    
+import { v4 as uuidv4 } from 'uuid';          
 import InputList from './components/inputList';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [inputList, setInputList] = useState([{ "id": 0, "inputId": "example id", "inputValue": "example value" }]);
+  const [inputList, setInputList] = useState([{ "id": uuidv4(), "inputId": "example id", "inputValue": "example value" }]);
 
   // const submitHandler = () => {
   //   console.log(signInRef.current?.value);
@@ -36,27 +36,20 @@ function App() {
     });
   };
 
-  const addInputContainer = () => {
-    setInputList((pre) => {
-      const currentLength = pre.length;
-
-      return [...pre, { "id": currentLength, "inputId": "example id", "inputValue": "example value"}];
-    });
+  const addInput = () => {
+    setInputList((pre) =>  [ ...pre, { "id": uuidv4(), "inputId": "example id", "inputValue": "example value" } ]);
   };
 
   return (
     <>
       <h1>Auto Filler</h1>
       <InputList inputList={inputList} setInputList={setInputList} />
-
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button id="save" onClick={() => save()}>Save</button>
-        <button id="get-all" onClick={() => getAllLocalItems()}>Get All</button>
+        <button id="save-btn" onClick={save}>Save</button>
+        <button id="get-all-btn" onClick={getAllLocalItems}>Get All</button>
         {/* <button onClick={() => submitHandler()}>Submit</button> */}
-        <button id="add-new-input-container" onClick={() => addInputContainer()}>Add Input Container</button>
+        <button id="add-new-input-btn" onClick={addInput}>Add Input Container</button>
+        {/* <button id="delete-input-btn" onClick={deleteInput}></button> */}
       </div>
     </>
   )
--- src/components/inputList.tsx
+++ src/components/inputList.tsx
@@ -30,6 +30,10 @@ function InputList(props: InputListProps) {
     });
   };
 
+  const handleDeleteInput = (id: number) => {
+    setInputList((pre: InputItemType[]) => pre.filter(item => item.id !== id));
+  };
+
   return (
     <div className="input-list-container">
       {inputList.map((item, index) => {
@@ -38,6 +42,9 @@ function InputList(props: InputListProps) {
             <div>Number {item.id + 1}</div>
             <input value={item.inputId} type="text" id={`input_id_${index}`} className="input-id" onChange={(e) => handleChangeInput('inputId', item.id, e.target.value)} />
             <input value={item.inputValue} type="text" id={`input_value_${index}`} className="input-value" onChange={(e) => handleChangeInput('inputValue', item.id, e.target.value)} />
+            <div onClick={() => handleDeleteInput(item.id)}>
+              Delete
+            </div>
           </div>
         );
       })}
